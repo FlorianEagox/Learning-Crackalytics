bb24b3c5348711a19d15f0fae392c7c238899bad
Seth Painter <timytomy11@gmail.com>
2019-10-16 17:39:38 -0500

    added selenium webdrivers and code to load the pearson page and login.

--- LearningCrackalytics.py
+++ LearningCrackalytics.py
@@ -1,6 +1,13 @@
 import yaml
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 with open('config.yaml', 'r') as file:
 	config = yaml.safe_load(file)
 
-print(config)
+browser = webdriver.Firefox()
+
+# Signing in
+browser.get(config['url'])  # Open the Learning Catalytics Page
+browser.find_element_by_css_selector("#user_username").send_keys(config['username'])
+browser.find_element_by_css_selector("#user_password").send_keys(config['password'] + Keys.RETURN)
