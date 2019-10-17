82e1f608b507e01849e8999615d564f6adb39450
Seth Painter <timytomy11@gmail.com>
2019-10-16 19:05:32 -0500

    added the logic to join the first session and to not select a seat (this is all very prototypical and should only take you through to start a session.

--- LearningCrackalytics.py
+++ LearningCrackalytics.py
@@ -1,5 +1,6 @@
 import yaml
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 
 with open('config.yaml', 'r') as file:
@@ -11,3 +12,12 @@ browser = webdriver.Firefox()
 browser.get(config['url'])  # Open the Learning Catalytics Page
 browser.find_element_by_css_selector("#user_username").send_keys(config['username'])
 browser.find_element_by_css_selector("#user_password").send_keys(config['password'] + Keys.RETURN)
+
+# Select the current session
+try:
+	browser.find_element_by_css_selector(".join_class_session_link").click()  # Click on the ongoing session
+except NoSuchElementException:
+	print('no sessions are taking place right now... Trying again')
+
+# Don't select a seat
+browser.find_element_by_link_text("I can't find my seat").click()
