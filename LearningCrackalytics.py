1d59b10286a2e330b9dd53b89864b3525efe13b7
Seth Painter <timytomy11@gmail.com>
2019-10-20 06:42:39 -0500

    MVP STATUS!!! The program will schedule specified times, run a session when it's time, and navigate through pearson website to do your questions!

--- LearningCrackalytics.py
+++ LearningCrackalytics.py
@@ -1,55 +1,88 @@
-import yaml
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
-import requests
 import json
+import time
+import requests
+import schedule
+import yaml
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 with open('config.yaml', 'r') as file:
 	config = yaml.safe_load(file)
 
 knowledge_base = {}
-browser = None  # webdriver.Firefox()
+switch_dates = {'MON': schedule.every().monday,
+				'TUE': schedule.every().monday,
+				'WED': schedule.every().wednesday,
+				'THU': schedule.every().thursday,
+				'FRI': schedule.every().friday,
+				'SAT': schedule.every().saturday,
+				'SUN': schedule.every().sunday}
 
 
-def minify(string): return "".join(e for e in string if e.isalnum()).lower()
+def minify(string): return ''.join(e for e in string if e.isalnum()).lower()
 
 
-def navigate_to_session():
-	# Signing in
+def navigate_session():
+	refresh = 15
+	browser = webdriver.Firefox(executable_path=config['geckodriver_path'], keep_alive=False)
 	browser.get(config['url'])  # Open the Learning Catalytics Page
-	browser.find_element_by_css_selector("#user_username").send_keys(config['username'])
-	browser.find_element_by_css_selector("#user_password").send_keys(config['password'] + Keys.RETURN)
+	if browser.find_elements_by_id('user_username') and browser.find_elements_by_id('user_password'):
+		browser.find_element_by_id('user_username').send_keys(config['username'])
+		browser.find_element_by_id('user_password').send_keys(config['password'] + Keys.RETURN)
+	session_active = True
+	while session_active:
+		if browser.current_url.split('/')[-1] == 'class_sessions':
+			# Select the current session
+			sessions = browser.find_elements_by_css_selector('.join_class_session_link')
+			if sessions:
+				sessions[0].click()  # Click on the ongoing session
+				refresh = 5
+			else:
+				browser.refresh()
+		elif browser.current_url.split('/')[-1] == 'select_seat':
+			browser.find_element_by_link_text("I can't find my seat").click()
+		elif browser.current_url.split('/')[-1].isnumeric():
+			if browser.find_elements_by_id('responses'):
+				question = browser.find_element_by_css_selector('.item_prompt_container label').text
+				answer = ask_question(question)
+				print(answer)
+				for choice in browser.find_elements_by_css_selector('.multiplechoice p'):
+					if minify(choice.text) in answer or answer in minify(choice.text):
+						choice.click()
+			else:
+				print('waiting for question')
+		elif browser.current_url.split('/')[-1] == 'post_session':
+			session_active = False
+			print(browser.find_element_by_id('score_summary').text)
+		time.sleep(refresh)
 
-	# Select the current session
-	try:
-		browser.find_element_by_css_selector(".join_class_session_link").click()  # Click on the ongoing session
-	except NoSuchElementException:
-		print('no sessions are taking place right now... Trying again')
 
-	# Don't select a seat
-	browser.find_element_by_link_text("I can't find my seat").click()
-
-
-def find_answer_from_quizlet(url, question):
+def find_answer_from_quizlet(url):
 	page = BeautifulSoup(requests.get(url).content, 'html.parser')
-	for el in page.select('.SetPageTerm-content'):
-		if minify(question) in minify(el.get_text()) and minify(question) not in knowledge_base:
-			knowledge_base[minify(question)] = minify(el.select('*')[-1].get_text())
+	for el in [el.select('*') for el in page.select('.SetPageTerm-content')]:
+		knowledge_base[minify(el[0].get_text())] = minify(el[-1].get_text())
 
 
 def get_search_results(question):
-	querystring = {'cx': config['cse_cx'], "key": config['cse_key'], 'q': question}
-	results = json.loads(requests.request("GET", config['cse_request_url'], params=querystring).text)
+	querystring = {'cx': config['cse_cx'], 'key': config['cse_key'], 'q': question}
+	results = json.loads(requests.request('GET', config['cse_request_url'], params=querystring).text)
 	return [item['link'] for item in results['items']]
 
 
-# navigate_to_session()
-question = 'On Aug. 5, 2017, Venus is in Gemini from Earth. If you stand on Venus, which constellation is Earth in'
-answer = ""
-for url in get_search_results(question):
-	if minify(question) not in knowledge_base:
-		find_answer_from_quizlet(url, question)
-		print(knowledge_base)
-print(knowledge_base[minify(question)])
+def ask_question(question):
+	found_answer = []
+	for url in get_search_results(question):
+		found_answer = [key for key in knowledge_base if minify(question) in key]
+		if not found_answer:
+			find_answer_from_quizlet(url)
+	if found_answer:
+		return knowledge_base[found_answer[0]]
+	return found_answer
+
+navigate_session()
+for times in config['times']:
+	switch_dates[times['day']].at(times['time']).do(navigate_session)
+while True:
+	schedule.run_pending()
+	time.sleep(1)
